d74b51ed411eeb3a4dc04b4da5e39df8a001e32f
anastasiia.tarasiuk <anastasiia.tarasiuk@iqvia.com>
2024-02-06 15:34:41 +0200

    Adds players tabs logic

--- dist/index.975ef6c8.js
+++ dist/index.975ef6c8.js
@@ -44586,14 +44586,64 @@ async function renderPlayersData() {
 function playersTemplate(players) {
     players.forEach((player)=>{
         const playerItem = document.createElement("li");
-        getStats(player.id);
+        const stats = getStats(player.id);
+        const games = getPersonalGameStats(stats);
+        // const bestScore =
+        console.log(33, games);
         playerItem.innerHTML = `   
             <button class="accordion">${player.name}</button>
             <div class="panel">
-                <p>No played games</p>
+                <!-- Tab links -->
+                <div class="tab">
+                  <button class="tablinks games" id="defaultOpen">Played Games</button>
+                  <button class="tablinks paris">Statistics</button>
+                  <button class="tablinks tokyo">Tokyo</button>
+                </div>
+                
+                <!-- Tab content -->
+                <ul id="gamesId" class="tabcontent">
+<!--                  <h3>London</h3>-->
+<!--                  <p>London is the capital city of England.</p>-->
+                  
+                  
+                </ul>
+                
+                <div id="Paris" class="tabcontent">
+                  <h3>Paris</h3>
+                  <p>Paris is the capital of France.</p>
+                </div>
+                
+                <div id="Tokyo" class="tabcontent">
+                  <h3>Tokyo</h3>
+                  <p>Tokyo is the capital of Japan.</p>
+                </div>
             </div>`;
         playerItem.querySelector(".accordion").addEventListener("click", (e)=>toggleAccordion(e));
+        playerItem.querySelector(".games").addEventListener("click", (e)=>openCity(e, "gamesId", playerItem));
+        playerItem.querySelector(".paris").addEventListener("click", (e)=>openCity(e, "Paris", playerItem));
+        playerItem.querySelector(".tokyo").addEventListener("click", (e)=>openCity(e, "Tokyo", playerItem));
+        const gameList = playerItem.querySelector("#gamesId");
+        let bestScore = 0;
+        let sum = 0;
+        games.forEach((game)=>{
+            for(const gameKey in game){
+                game[gameKey].forEach((session)=>{
+                    const score = Number(session.score);
+                    if (score > bestScore) bestScore = score;
+                    sum += score;
+                });
+                docData.games.forEach((docDataGame)=>{
+                    if (docDataGame.id === gameKey) {
+                        const gameListItem = document.createElement("li");
+                        gameListItem.classList.add("game-list-item");
+                        gameListItem.innerHTML = `<div><p>${docDataGame.name}</p><img class="thumbnail" src=${docDataGame.url}><p>Best score: ${bestScore}</p><p>Average score: ${sum / games.length}</p><p>Plays: ${games.length}</p></div>`;
+                        gameList.insertAdjacentElement("beforeend", gameListItem);
+                    }
+                });
+            }
+        });
         playersEl.insertAdjacentElement("beforeend", playerItem);
+        playersEl.querySelectorAll("#defaultOpen").forEach((item)=>item.click());
     });
 }
 function toggleAccordion(e) {
@@ -44604,8 +44654,36 @@ function toggleAccordion(e) {
     else panel.style.display = "block";
 }
 function getStats(playerId) {
-    const stats = {};
-    for (const play of docData.plays)if (play.playerId === playerId.toString()) console.log(play);
+    const stats = [];
+    for (const play of docData.plays)if (play.playerId === playerId.toString()) stats.push(play);
+    return stats;
+}
+function openCity(e, city, parent) {
+    // Get all elements with class="tabcontent" and hide them
+    const tabcontent = parent.querySelectorAll(".tabcontent");
+    for(let i = 0; i < tabcontent.length; i++)tabcontent[i].style.display = "none";
+    // Get all elements with class="tablinks" and remove the class "active"
+    const tablinks = parent.querySelectorAll(".tablinks");
+    for(let i1 = 0; i1 < tablinks.length; i1++)tablinks[i1].className = tablinks[i1].className.replace("active", "");
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    parent.querySelector(`#${city}`).style.display = "block";
+    e.currentTarget.className += " active";
+}
+function getPersonalGameStats(stats) {
+    const games = [];
+    const ids = [];
+    for (const play of stats)if (!ids.includes(play.gameId)) {
+        const game = {
+            [play.gameId]: [
+                play
+            ]
+        };
+        ids.push(play.gameId);
+        games.push(game);
+    } else games.forEach((game)=>{
+        if (game[play.gameId] === play.gameId) game[play.gameId].push(play);
+    });
+    return games;
 }
 
 },{"firebase/auth":"79vzg","./login":"47T64","./game_list":"1RWSs"}]},["1RB6v","8lqZg"], "8lqZg", "parcelRequired7c6")
--- src/js/players.js
+++ src/js/players.js
@@ -6,6 +6,7 @@ const playersEl = document.querySelector(".players");
 
 let docData = null;
 
+
 if (playersEl) {
     renderPlayersData();
 }
@@ -24,16 +25,74 @@ function playersTemplate(players) {
     players.forEach(player => {
         const playerItem = document.createElement("li");
 
-        getStats(player.id);
+        const stats = getStats(player.id);
+
+        const games = getPersonalGameStats(stats);
+
+        // const bestScore =
+        console.log(33, games)
 
         playerItem.innerHTML = `   
             <button class="accordion">${player.name}</button>
             <div class="panel">
-                <p>No played games</p>
+                <!-- Tab links -->
+                <div class="tab">
+                  <button class="tablinks games" id="defaultOpen">Played Games</button>
+                  <button class="tablinks paris">Statistics</button>
+                  <button class="tablinks tokyo">Tokyo</button>
+                </div>
+                
+                <!-- Tab content -->
+                <ul id="gamesId" class="tabcontent">
+<!--                  <h3>London</h3>-->
+<!--                  <p>London is the capital city of England.</p>-->
+                  
+                  
+                </ul>
+                
+                <div id="Paris" class="tabcontent">
+                  <h3>Paris</h3>
+                  <p>Paris is the capital of France.</p>
+                </div>
+                
+                <div id="Tokyo" class="tabcontent">
+                  <h3>Tokyo</h3>
+                  <p>Tokyo is the capital of Japan.</p>
+                </div>
             </div>`;
         playerItem.querySelector(".accordion").addEventListener("click", e => toggleAccordion(e));
+        playerItem.querySelector(".games").addEventListener("click", e => openCity(e, 'gamesId', playerItem));
+        playerItem.querySelector(".paris").addEventListener("click", e => openCity(e, 'Paris', playerItem));
+        playerItem.querySelector(".tokyo").addEventListener("click", e => openCity(e, 'Tokyo', playerItem));
+
+        const gameList = playerItem.querySelector("#gamesId");
+
+        let bestScore = 0;
+        let sum = 0;
+
+        games.forEach(game => {
+            for (const gameKey in game) {
+                game[gameKey].forEach(session => {
+                    const score = Number(session.score);
+                    if (score > bestScore) {
+                        bestScore = score;
+                    }
+                    sum += score;
+                })
+
+                docData.games.forEach(docDataGame => {
+                    if (docDataGame.id === gameKey) {
+                        const gameListItem = document.createElement("li");
+                        gameListItem.classList.add("game-list-item");
+                        gameListItem.innerHTML =`<div><p>${docDataGame.name}</p><img class="thumbnail" src=${docDataGame.url}><p>Best score: ${bestScore}</p><p>Average score: ${sum/games.length}</p><p>Plays: ${games.length}</p></div>`
+                        gameList.insertAdjacentElement("beforeend", gameListItem);
+                    }
+                })
+            }
+        })
 
         playersEl.insertAdjacentElement("beforeend", playerItem);
+        playersEl.querySelectorAll("#defaultOpen").forEach(item => item.click());
     })
 }
 
@@ -50,10 +109,54 @@ function toggleAccordion(e) {
 }
 
 function getStats(playerId) {
-    const stats = {};
+    const stats = [];
     for (const play of docData.plays) {
         if (play.playerId === playerId.toString()) {
-            console.log(play)
+            stats.push(play);
         }
     }
+    return stats;
+}
+
+function openCity(e, city, parent) {
+    // Get all elements with class="tabcontent" and hide them
+    const tabcontent = parent.querySelectorAll(".tabcontent");
+
+    for (let i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    const tablinks = parent.querySelectorAll(".tablinks");
+
+    for (let i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace("active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    parent.querySelector(`#${city}`).style.display = "block";
+    e.currentTarget.className += " active";
+}
+
+function getPersonalGameStats(stats) {
+    const games = [];
+    const ids = [];
+
+    for (const play of stats) {
+        if (!ids.includes(play.gameId)) {
+            const game = {
+                [play.gameId]: [play]
+            }
+            ids.push(play.gameId);
+            games.push(game);
+        } else {
+            games.forEach(game => {
+                if (game[play.gameId] === play.gameId) {
+                    game[play.gameId].push(play);
+                }
+            })
+        }
+    }
+
+    return games;
 }
